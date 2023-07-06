a424b278ffb813c2f3082f66e3dca076c5c2031d
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2023-07-06 16:13:03 +0200

    Update nlu_lab11.py
    Final version (?)

--- nlu_lab11.py
+++ nlu_lab11.py
@@ -59,7 +59,7 @@ from tqdm import tqdm
 from nltk.corpus import movie_reviews
 nltk.download('movie_reviews')
 nltk.download('vader_lexicon')
-'''
+
 """## Load Data"""
 
 n_instances = 10000
@@ -68,8 +68,8 @@ subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories='subj')[:n
 obj_docs = [(sent, 'obj') for sent in subjectivity.sents(categories='obj')[:n_instances]]
 
 len(subj_docs), len(obj_docs)
-print(subj_docs[0])
-'''
+print("Sentence of Subjectivity dataset: ", subj_docs[0])
+
 
 """## Preprocessing Data"""
 
@@ -81,7 +81,7 @@ def preprocess_text(text):
     lemmatizer = WordNetLemmatizer()
     tokens = [lemmatizer.lemmatize(token) for token in tokens]  # Lemmatizzazione
     return tokens
-'''
+
 subj_prep = []
 obj_prep = []
 all_prep = []
@@ -96,9 +96,9 @@ for sent, label in obj_docs:
 
 all_prep = subj_prep + obj_prep
 
-print(len(subj_prep))
-print(len(obj_prep))
-print(len(all_prep))
+print("Subjective sentences: ", len(subj_prep))
+print("Objective sentences: ", len(obj_prep))
+print("All sentences: ", len(all_prep))
 print(subj_prep[0])
 print(subj_prep[1])
 
@@ -127,8 +127,8 @@ for sent, label in test_all:
   X_test.append(' '.join(sent))
   y_test.append(label)
 
-vectorizer = CountVectorizer()
-X_train_vectorized = vectorizer.fit_transform(X_train)
+subj_vectorizer = CountVectorizer()
+X_train_vectorized = subj_vectorizer.fit_transform(X_train)
 
 """## Subjectivity - MLP Model"""
 
@@ -136,12 +136,10 @@ model = MLPClassifier(hidden_layer_sizes=(100, 50), activation='relu', solver='a
 model.fit(X_train_vectorized, y_train)
 
 # Valutazione del modello
-X_test_vectorized = vectorizer.transform(X_test)
-predictions = model.predict(X_test_vectorized)
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy:", accuracy)
-
-"""## Polarity - MLP Model"""
+X_test_vectorized = subj_vectorizer.transform(X_test)
+subj_predictions = model.predict(X_test_vectorized)
+subj_accuracy = accuracy_score(y_test, subj_predictions)
+print("[SUBJECTIVITY] Accuracy with no K-fold:", subj_accuracy)
 
 x = X_train + X_test
 y = y_train + y_test
@@ -150,7 +148,7 @@ vectorizer = CountVectorizer()
 
 clf = MLPClassifier()
 
-skf = StratifiedKFold(n_splits=10, random_state=42, shuffle=True)
+skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
 
 accuracies = []
 
@@ -170,54 +168,9 @@ for train_index, test_index in tqdm(skf.split(x, y)):
 
 mean_accuracy = sum(accuracies) / len(accuracies)
 
-print("Mean Accuracy:", mean_accuracy)
+print("[SUBJECTIVITY] Mean Accuracy - All sentences: ", mean_accuracy)
 
-
-X_train_subj = []
-y_train_subj = []
-X_test_subj = []
-y_test_subj = []
-
-for sent, label in train_subj:
-  X_train_subj.append(' '.join(sent))
-  y_train_subj.append(label)
-
-for sent, label in test_subj:
-  X_test_subj.append(' '.join(sent))
-  y_test_subj.append(label)
-
-vectorizer = CountVectorizer()
-X_train__subj_vectorized = vectorizer.fit_transform(X_train_subj)
-
-x = X_train_subj + X_test_subj
-y = y_train_subj + y_test_subj
-
-vectorizer = CountVectorizer()
-
-clf = MLPClassifier()
-
-skf = StratifiedKFold(n_splits=10, random_state=42, shuffle=True)
-
-accuracies = []
-
-for train_index, test_index in tqdm(skf.split(x, y)):
-    x_train_fold, x_test_fold = [x[i] for i in train_index], [x[i] for i in test_index]
-    y_train_fold, y_test_fold = [y[i] for i in train_index], [y[i] for i in test_index]
-
-    train_features = vectorizer.fit_transform(x_train_fold)
-    test_features = vectorizer.transform(x_test_fold)
-
-    clf.fit(train_features, y_train_fold)
-
-    predictions = clf.predict(test_features)
-
-    accuracy = accuracy_score(y_test_fold, predictions)
-    accuracies.append(accuracy)
-
-mean_accuracy = sum(accuracies) / len(accuracies)
-
-print("Mean Accuracy:", mean_accuracy)
-'''
+"""## [POLARITY] Load Data"""
 
 movie_sentences = movie_reviews.sents()
 polarity_all = []
@@ -227,13 +180,8 @@ for sentence in tqdm(movie_sentences):
   sentence_str = ' '.join(sentence)
   ss = sid.polarity_scores(sentence_str)
   polarity_all.append(ss)
-  #print(sentence_str, ss)
-  #print()
 
-print(type(movie_sentences))
-print(type(polarity_all))
-
-##[POLARITY] Preprocessing Data
+"""##[POLARITY] Preprocessing Data"""
 
 movie_prep = []
 
@@ -241,14 +189,12 @@ for sent in tqdm(movie_sentences):
     preprocessed_sentence = preprocess_text(' '.join(sent))
     movie_prep.append(preprocessed_sentence)
 
-print(len(movie_prep))
-print(movie_prep[0])
+print("Length movie dataset: ", len(movie_prep))
+print("Example: ", movie_prep[0])
 
-##[POLARITY] Split Data
+"""##[POLARITY] Split Data"""
 
 movie_train, movie_test = train_test_split(movie_prep, test_size=0.2, random_state=42)
-
-print(len(movie_train), len(movie_test))
 
 polarity_train = []
 polarity_test = []
@@ -276,62 +222,135 @@ for sentence in tqdm(movie_test):
   polarity_test.append(pol)
 
 print()
-print("TRAINING VALUES:")
+print("MOVIES TRAINING VALUES:")
 print(len(polarity_train))
 values = pd.Series(polarity_train).value_counts()
 print(values)
 print()
-print("TEST VALUES:")
+print("MOVIES TEST VALUES:")
 print(len(polarity_test))
 values = pd.Series(polarity_test).value_counts()
 print(values)
 
-X_train = []
-y_train = []
-X_test = []
-y_test = []
+X_train_movie = []
+y_train_movie = []
+X_test_movie = []
+y_test_movie = []
 
 for sent, polarity in zip(movie_train, polarity_train):
-  X_train.append(' '.join(sent))
-  y_train.append(polarity)
+  X_train_movie.append(' '.join(sent))
+  y_train_movie.append(polarity)
 
 for sent, polarity in zip(movie_test, polarity_test):
-  X_test.append(' '.join(sent))
-  y_test.append(polarity)
+  X_test_movie.append(' '.join(sent))
+  y_test_movie.append(polarity)
 
-print(len(X_train), len(X_test))
-print(len(y_test))
 
-vectorizer = CountVectorizer()
-X_train_vectorized = vectorizer.fit_transform(X_train)
+polarity_vectorizer = CountVectorizer()
+X_train_vectorized = polarity_vectorizer.fit_transform(X_train_movie)
 
-## [POLARITY] MLP Model
+"""## [POLARITY] MLP Model"""
 
-x = X_train + X_test
-y = y_train + y_test
+x_movie = X_train_movie + X_test_movie
+y_movie = y_train_movie + y_test_movie
 
 vectorizer = CountVectorizer()
 
-clf = MLPClassifier(hidden_layer_sizes=(100, 50), activation='relu', solver='adam')
+model_polarity = MLPClassifier(hidden_layer_sizes=(100, 50), activation='relu', solver='adam')
 
-skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+skf_polarity = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
 
-accuracies = []
+accuracies_polarity = []
 
-for train_index, test_index in tqdm(skf.split(x, y), total=skf.get_n_splits()):
-    x_train_fold, x_test_fold = [x[i] for i in train_index], [x[i] for i in test_index]
-    y_train_fold, y_test_fold = [y[i] for i in train_index], [y[i] for i in test_index]
+for train_index, test_index in tqdm(skf_polarity.split(x, y), total=skf_polarity.get_n_splits()):
+    x_train_fold, x_test_fold = [x_movie[i] for i in train_index], [x_movie[i] for i in test_index]
+    y_train_fold, y_test_fold = [y_movie[i] for i in train_index], [y_movie[i] for i in test_index]
 
     train_features = vectorizer.fit_transform(x_train_fold)
     test_features = vectorizer.transform(x_test_fold)
 
-    clf.fit(train_features, y_train_fold)
+    model_polarity.fit(train_features, y_train_fold)
 
-    predictions = clf.predict(test_features)
+    predictions = model_polarity.predict(test_features)
 
     accuracy = accuracy_score(y_test_fold, predictions)
-    accuracies.append(accuracy)
+    accuracies_polarity.append(accuracy)
 
-mean_accuracy = sum(accuracies) / len(accuracies)
+mean_accuracy_polarity = sum(accuracies_polarity) / len(accuracies_polarity)
 
-print("[POLARITY] Mean Accuracy - All sentences: ", mean_accuracy)
+print("[POLARITY] Mean Accuracy - All sentences: ", mean_accuracy_polarity)
+
+
+""" ##Remove Objective Sentences and obtain new accuracy """
+
+polarity_all = polarity_train + polarity_test
+movie_all = movie_train + movie_test
+
+movie_temp = []
+
+for sent in movie_all:
+  movie_temp.append(' '.join(sent))
+
+movie_all = movie_temp
+
+movie_all_vectorized = subj_vectorizer.transform(movie_all)
+movie_subj_pred = model.predict(movie_all_vectorized)
+
+movie_no_obj = []
+
+for sent, label in zip(movie_all, movie_subj_pred):
+  if label == 'subj':
+    movie_no_obj.append(sent)
+
+print("Length of movie subjective sentences: ", len(movie_no_obj))
+
+movie_temp = []
+
+for sent in movie_no_obj:
+  movie_temp.append(sent.split())
+
+movie_no_obj = movie_temp
+
+polarity_no_obj = []
+
+for sentence in tqdm(movie_no_obj):
+  sentence_str = ' '.join(sentence)
+  ss = sid.polarity_scores(sentence_str)
+  if(ss['compound'] == 0):
+    pol = 'Neu'
+  elif(ss['compound'] > 0):
+    pol = 'Pos'
+  else:
+    pol = 'Neg'
+  polarity_no_obj.append(pol)
+
+print("Length of movie subjective polarity: ", len(polarity_no_obj))
+counts = pd.Series(polarity_no_obj).value_counts()
+print(counts)
+
+movie_temp = []
+
+for sent in movie_no_obj:
+  movie_temp.append(' '.join(sent))
+
+movie_no_obj = movie_temp
+
+accuracies_polarity_no_obj = []
+
+for train_index, test_index in tqdm(skf_polarity.split(movie_no_obj, polarity_no_obj), total=skf_polarity.get_n_splits()):
+    x_train_fold, x_test_fold = [movie_no_obj[i] for i in train_index], [movie_no_obj[i] for i in test_index]
+    y_train_fold, y_test_fold = [polarity_no_obj[i] for i in train_index], [polarity_no_obj[i] for i in test_index]
+
+    train_features = polarity_vectorizer.fit_transform(x_train_fold)
+    test_features = polarity_vectorizer.transform(x_test_fold)
+
+    model_polarity.fit(train_features, y_train_fold)
+
+    predictions_no_obj = model_polarity.predict(test_features)
+
+    accuracy_no_obj = accuracy_score(y_test_fold, predictions_no_obj)
+    accuracies_polarity_no_obj.append(accuracy_no_obj)
+
+mean_accuracy_polarity_no_obj = sum(accuracies_polarity_no_obj) / len(accuracies_polarity_no_obj)
+
+print("[POLARITY] Mean Accuracy - Without objective sentences: ", mean_accuracy_polarity_no_obj)
