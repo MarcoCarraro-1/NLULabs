90e89ba7e4db615babf743e06c568a24bf3fd210
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2023-07-10 09:53:43 +0200

    Update nlu_lab8.py
    Remove description ex

--- nlu_lab8.py
+++ nlu_lab8.py
@@ -8,13 +8,6 @@ Original file is located at
 
 
 """# Lab Exercise
-**Same test set for all the experiments, you can use K-fold validation**
-
-* Extend collocational features with
-  * POS-tags
-  * Ngrams within window
-* Concatenate BOW and new collocational feature vectors & evaluate
-* Evaluate Lesk Original and Graph-based (Lesk Similarity or Pedersen) metrics on the same test split and compare
 
 ## Import & Installing
 """
