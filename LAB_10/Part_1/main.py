21bd31608ac38a1fc2e84d7d7e44496d0de7a191
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2023-07-14 16:39:44 +0200

    Update main.py
    Adjust PAD_TOKEN

--- LAB_10/Part_1/main.py
+++ LAB_10/Part_1/main.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     
     train_raw, dev_raw, test_raw, y_train, y_dev, y_test = load_data('dataset/ATIS/train.json', 
                                                          'dataset/ATIS/test.json')
-    lang = get_lang(train_raw, dev_raw, test_raw, y_train, y_dev, y_test, PAD_TOKEN)
+    lang = get_lang(train_raw, dev_raw, test_raw, y_train, y_dev, y_test)
     train_dataset, dev_dataset, test_dataset = get_dataset(train_raw, dev_raw, test_raw, lang)
     train_loader, dev_loader, test_loader = get_dataload(train_dataset, dev_dataset, 
                                                          test_dataset)
     
-    f1_bidir, intent_acc_bidir = eval_f1_acc(lang, train_loader, dev_loader, test_loader, PAD_TOKEN, 0)
+    f1_bidir, intent_acc_bidir = eval_f1_acc(lang, train_loader, dev_loader, test_loader, 0)
     
-    f1_dropout, intent_acc_dropout = eval_f1_acc(lang, train_loader, dev_loader, test_loader, PAD_TOKEN, 1)
+    f1_dropout, intent_acc_dropout = eval_f1_acc(lang, train_loader, dev_loader, test_loader, 1)
