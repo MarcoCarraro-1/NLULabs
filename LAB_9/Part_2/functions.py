1d646f674d112287ff368c5b8b7402cdad6cd068
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2023-07-14 11:47:33 +0200

    Add files via upload

---
+++ LAB_9/Part_2/functions.py
@@ -0,0 +1,107 @@
+from utils import *
+from model import *
+import numpy as np
+from numpy.linalg import norm
+from scipy.spatial.distance import cosine
+from scipy.spatial.distance import cosine
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import math
+import torch.utils.data as data
+from functools import partial
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import copy
+from torch.optim import AdamW
+
+
+def load_data(train_fold, valid_fold, test_fold):
+    train_raw = read_file(train_fold)
+    dev_raw = read_file(valid_fold)
+    test_raw = read_file(test_fold)
+
+    vocab = get_vocab(train_raw, ["<pad>", "<eos>"])
+
+    lang = Lang(train_raw, ["<pad>", "<eos>"])
+
+    train_dataset = PennTreeBank(train_raw, lang)
+    dev_dataset = PennTreeBank(dev_raw, lang)
+    test_dataset = PennTreeBank(test_raw, lang)
+
+    train_loader = DataLoader(train_dataset, batch_size=64, collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"]),  shuffle=True)
+    dev_loader = DataLoader(dev_dataset, batch_size=64, collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"]))
+    test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"]))
+    
+    return vocab, lang, train_loader, dev_loader, test_loader
+
+
+
+def eval_ppl(lang, train_loader, dev_loader, test_loader, call):
+    hid_size = 250
+    emb_size = 400
+    torch.cuda.empty_cache()
+    
+    device = 'cuda:0'
+    vocab_len = len(lang.word2id)
+    if call==0:
+        model = LM_LSTM(emb_size, hid_size, vocab_len, pad_index=lang.word2id["<pad>"], tie_weights=True).to('cuda:0')
+    elif call==1:
+        model = LM_LSTM_VarDrop(emb_size, hid_size, vocab_len, call, pad_index=lang.word2id["<pad>"], tie_weights=True).to('cuda:0')
+        
+    model.apply(init_weights)
+    
+    if call == 0 or call == 1:
+        lr = 0.0001
+        optimizer = AdamW(model.parameters(), lr=lr, weight_decay=0.01, eps=1e-6)
+    else:
+        lr = 30
+        optimizer = torch.optim.ASGD(model.parameters(), lr=lr, t0=0, lambd=0., weight_decay=1.2e-6)
+    
+    clip = 0.25
+    decay = 1.2e-6
+    mu = 0.9
+    epsilon = 1e-8
+    
+    criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
+    criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
+
+    n_epochs = 100
+    patience = 3
+    losses_train = []
+    losses_dev = []
+    sampled_epochs = []
+    best_ppl = math.inf
+    best_model = None
+    pbar = tqdm(range(1,n_epochs))
+    for epoch in pbar:
+        loss = train_loop(train_loader, optimizer, criterion_train, model, clip)
+
+        if epoch % 1 == 0:
+            sampled_epochs.append(epoch)
+            losses_train.append(np.asarray(loss).mean())
+            ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
+            losses_dev.append(np.asarray(loss_dev).mean())
+            pbar.set_description("PPL: %f" % ppl_dev)
+            if  ppl_dev < best_ppl:
+                best_ppl = ppl_dev
+                best_model = copy.deepcopy(model).to('cpu')
+                patience = 3
+            else:
+                patience -= 1
+
+            if patience <= 0:
+                break 
+
+    best_model.to('cuda:0')
+    final_ppl,  _ = eval_loop(test_loader, criterion_eval, best_model)
+    if call == 0:
+        print("Weight Tying: ")
+    elif call == 1:
+        print("Variational Dropout: ")
+    else:
+        print("Non Monotonically Triggered AvSGD: ")
+    print()
+    print('Test ppl: ', final_ppl)
---
+++ LAB_9/Part_2/model.py
@@ -0,0 +1,82 @@
+from functions import *
+from utils import *
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+
+class LM_LSTM(nn.Module):
+    def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.4,
+                 emb_dropout=0.1, n_layers=1, tie_weights=False):
+        super(LM_LSTM, self).__init__()
+        self.encoder = nn.Embedding(output_size, emb_size)
+        self.decoder = nn.Linear(hidden_size, output_size)
+        if tie_weights:
+            self.decoder.weight = self.encoder.weight
+
+        self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
+        self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False)
+        self.pad_token = pad_index
+        self.output = nn.Linear(hidden_size, output_size)
+        self.tie_weights = tie_weights
+
+    def forward(self, input_sequence):
+        emb = self.embedding(input_sequence)
+        #emb = self.dropout_emb(emb)
+        lstm_out, _  = self.lstm(emb)
+        #lstm_out = self.dropout_out(lstm_out)
+        output = self.output(lstm_out).permute(0,2,1)
+        return output
+    
+    def get_word_embedding(self, token):
+        return self.embedding(token).squeeze(0).detach().cpu().numpy()
+
+    def get_most_similar(self, vector, top_k=10):
+        embs = self.embedding.weight.detach().cpu().numpy()
+        scores = []
+        for i, x in enumerate(embs):
+            if i != self.pad_token:
+                scores.append(cosine_similarity(x, vector))
+        # Take ids of the most similar tokens
+        scores = np.asarray(scores)
+        indexes = np.argsort(scores)[::-1][:top_k]
+        top_scores = scores[indexes]
+        return (indexes, top_scores)
+    
+    
+class VarDrop(nn.Module):
+    def __init__(self, p=0.5):
+        super().__init__()
+        self.p = p
+        self.mask = None
+
+    def forward(self, x):
+        if not self.training:
+            return x
+
+        if self.mask is None:
+            self.mask = x.new_empty(1, x.size(1), requires_grad=False).bernoulli_(1 - self.p)
+
+        return x * self.mask.div_(1 - self.p)
+    
+    
+class LM_LSTM_VarDrop(nn.Module):
+    def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1,
+                 emb_dropout=0.1, n_layers=1):
+        super(LM_LSTM_VarDrop, self).__init__()
+        self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
+        self.emb_dropout = VarDrop(emb_dropout)
+        self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False)    
+        self.out_dropout = VarDrop(out_dropout)
+        self.pad_token = pad_index
+        self.output = nn.Linear(hidden_size, output_size)
+        self.output.weight = self.embedding.weight  # Weight tying
+        
+    def forward(self, input_sequence):
+        emb = self.embedding(input_sequence)
+        emb = self.emb_dropout(emb)
+        lstm_out, _  = self.lstm(emb)
+        lstm_out = self.out_dropout(lstm_out)
+        output = self.output(lstm_out).permute(0,2,1)
+        return output
