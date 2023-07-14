1062411c70e509b13a4f90cafde81bc5970454b5
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2023-07-14 16:52:10 +0200

    Update utils.py
    crtirnrion bug correct

--- LAB_10/Part_1/utils.py
+++ LAB_10/Part_1/utils.py
@@ -66,7 +66,7 @@ def init_weights(mat):
                     m.bias.data.fill_(0.01)
 
 
-def train_loop(data, optimizer, criterion_slots, critenrion_intents, model):
+def train_loop(data, optimizer, criterion_slots, criterion_intents, model):
     model.train()
     loss_array = []
     for sample in data:
