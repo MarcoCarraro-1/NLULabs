e5b1c995fdf7bebc1be76e534a7783607aefda79
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2023-07-15 16:54:05 +0200

    Update functions.py
    add bin folder to code

--- LAB_10/Part_1/functions.py
+++ LAB_10/Part_1/functions.py
@@ -181,9 +181,9 @@ def eval_f1_acc(lang, train_loader, dev_loader, test_loader, call):
                 if f1 > best_f1:
                     best_f1 = f1
                     if call == 0:
-                        torch.save(model.state_dict(), 'bidirectional_IAS.bin')
+                        torch.save(model.state_dict(), 'bin/bidirectional_IAS.bin')
                     else:
-                        torch.save(model.state_dict(), 'dropout_IAS.bin')
+                        torch.save(model.state_dict(), 'bin/dropout_IAS.bin')
                 else:
                     patience -= 1
                 if patience <= 0: # Early stoping with patient
